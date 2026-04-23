d355b54e542f290ca80d8e3e4c3d79343ed9c6c7
DasSkippy <sean.frederico105@gmail.com>
2026-04-23 12:39:28 -0700

    added the ability for enemies to spawn in the correct manner

--- Assets/Scripts/Levels/EnemySpawner.cs
+++ Assets/Scripts/Levels/EnemySpawner.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-using System.IO;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using System.Collections;
 using System.Linq;
-using RPNEvaluator;
+using System;
+using System.Reflection;
 
 [System.Serializable]
 public class Enemy
 {
     public string name;
+    public int sprite;
     public int hp;
     public int speed;
     public int damage;
@@ -31,6 +31,9 @@ public class Spawn
     public string enemy;
     public string count;
     public string hp;
+    public string speed;
+    public string damage;
+    public string delay;
     public int[] sequence;
     public string location;
 }
@@ -46,6 +49,7 @@ public class Spawn
     public List<Level> levels;
 
     Level currentLevel;
+    int currentWave;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -95,13 +99,14 @@ public class Spawn
     public void StartLevel(string levelname)
     {
         level_selector.gameObject.SetActive(false);
-        foreach (Level level in levels)
+        currentWave = 0;
+        currentLevel = levels.FirstOrDefault(level => level.name == levelname);
+
+        if (currentLevel == null)
         {
-            if(level.name == levelname)
-            {
-                currentLevel = level;
-                break;
-            }
+            level_selector.gameObject.SetActive(true);
+            Debug.LogWarning($"No level definition found for '{levelname}'.");
+            return;
         }
         // this is not nice: we should not have to be required to tell the player directly that the level is starting
         GameManager.Instance.player.GetComponent<PlayerController>().StartLevel();
@@ -110,12 +115,28 @@ public class Spawn
 
     public void NextWave()
     {
+        if (currentLevel == null)
+        {
+            return;
+        }
+
+        if (currentLevel.waves > 0 && currentWave >= currentLevel.waves)
+        {
+            return;
+        }
+
         StartCoroutine(SpawnWave());
     }
 
 
     IEnumerator SpawnWave()
     {
+        if (currentLevel.waves > 0 && currentWave >= currentLevel.waves)
+        {
+            yield break;
+        }
+
+        currentWave++;
         GameManager.Instance.state = GameManager.GameState.COUNTDOWN;
         GameManager.Instance.countdown = 3;
         for (int i = 3; i > 0; i--)
@@ -124,33 +145,157 @@ public class Spawn
             GameManager.Instance.countdown--;
         }
         GameManager.Instance.state = GameManager.GameState.INWAVE;
-        for (int i = 0; i < 10; ++i)
+
+        foreach (Spawn spawn in currentLevel.spawns)
         {
-            yield return SpawnEnemy();
+            yield return StartCoroutine(SpawnEnemyGroups(spawn));
         }
+
         yield return new WaitWhile(() => GameManager.Instance.enemy_count > 0);
         GameManager.Instance.state = GameManager.GameState.WAVEEND;
     }
 
-    IEnumerator SpawnZombie()
+    IEnumerator SpawnEnemyGroups(Spawn spawn)
     {
-        SpawnPoint spawn_point = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
-        Vector2 offset = Random.insideUnitCircle * 1.8f;
-                
-        Vector3 initial_position = spawn_point.transform.position + new Vector3(offset.x, offset.y, 0);
-        GameObject new_enemy = Instantiate(enemy, initial_position, Quaternion.identity);
+        Enemy enemyDefinition = enemies.FirstOrDefault(e => e.name == spawn.enemy);
+        if (enemyDefinition == null)
+        {
+            Debug.LogWarning($"No enemy definition found for '{spawn.enemy}'.");
+            yield break;
+        }
 
-        new_enemy.GetComponent<SpriteRenderer>().sprite = GameManager.Instance.enemySpriteManager.Get(0);
-        EnemyController en = new_enemy.GetComponent<EnemyController>();
-        en.hp = new Hittable(50, Hittable.Team.MONSTERS, new_enemy);
-        en.speed = 10;
-        GameManager.Instance.AddEnemy(new_enemy);
-        yield return new WaitForSeconds(0.5f);
+        int totalCount = EvaluateExpression(spawn.count, 0);
+        if (totalCount <= 0)
+        {
+            yield break;
+        }
+
+        int[] sequence = spawn.sequence == null || spawn.sequence.Length == 0 ? new[] { 1 } : spawn.sequence;
+        int delaySeconds = EvaluateExpression(spawn.delay, 2);
+        int hp = EvaluateExpression(spawn.hp, enemyDefinition.hp, enemyDefinition.hp);
+        int speed = EvaluateExpression(spawn.speed, enemyDefinition.speed, enemyDefinition.speed);
+        int damage = EvaluateExpression(spawn.damage, enemyDefinition.damage, enemyDefinition.damage);
+        int spawned = 0;
+        int sequenceIndex = 0;
+
+        while (spawned < totalCount)
+        {
+            int groupSize = Mathf.Min(sequence[sequenceIndex], totalCount - spawned);
+            SpawnPoint spawnPoint = GetSpawnPoint(spawn.location);
+
+            for (int i = 0; i < groupSize; i++)
+            {
+                SpawnEnemy(enemyDefinition, spawnPoint, hp, speed, damage);
+            }
+
+            spawned += groupSize;
+            sequenceIndex = (sequenceIndex + 1) % sequence.Length;
+
+            if (spawned < totalCount)
+            {
+                yield return new WaitForSeconds(delaySeconds);
+            }
+        }
     }
 
-    IEnumerator SpawnEnemy()
+    void SpawnEnemy(Enemy enemyDefinition, SpawnPoint spawnPoint, int hp, int speed, int damage)
     {
-        string[] spawnType = 
-        SpawnPoint spawn_point = SpawnPoints
+        Vector2 offset = UnityEngine.Random.insideUnitCircle * 1.8f;
+        Vector3 position = spawnPoint.transform.position + new Vector3(offset.x, offset.y, 0f);
+        GameObject newEnemy = Instantiate(enemy, position, Quaternion.identity);
+
+        newEnemy.GetComponent<SpriteRenderer>().sprite = GameManager.Instance.enemySpriteManager.Get(enemyDefinition.sprite);
+
+        EnemyController controller = newEnemy.GetComponent<EnemyController>();
+        controller.hp = new Hittable(hp, Hittable.Team.MONSTERS, newEnemy);
+        controller.speed = speed;
+        controller.damage = damage;
+
+        GameManager.Instance.AddEnemy(newEnemy);
+    }
+
+    SpawnPoint GetSpawnPoint(string location)
+    {
+        if (SpawnPoints == null || SpawnPoints.Length == 0)
+        {
+            throw new InvalidOperationException("EnemySpawner requires at least one spawn point.");
+        }
+
+        if (string.IsNullOrWhiteSpace(location) || location.Trim().ToLower() == "random")
+        {
+            return SpawnPoints[UnityEngine.Random.Range(0, SpawnPoints.Length)];
+        }
+
+        string[] parts = location.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length < 2)
+        {
+            return SpawnPoints[UnityEngine.Random.Range(0, SpawnPoints.Length)];
+        }
+
+        string requestedType = parts[1].ToUpper();
+        SpawnPoint.SpawnName spawnName;
+        if (!Enum.TryParse(requestedType, out spawnName))
+        {
+            return SpawnPoints[UnityEngine.Random.Range(0, SpawnPoints.Length)];
+        }
+
+        SpawnPoint[] matchingPoints = SpawnPoints.Where(point => point.kind == spawnName).ToArray();
+        if (matchingPoints.Length == 0)
+        {
+            return SpawnPoints[UnityEngine.Random.Range(0, SpawnPoints.Length)];
+        }
+
+        return matchingPoints[UnityEngine.Random.Range(0, matchingPoints.Length)];
+    }
+
+    int EvaluateExpression(string expression, int defaultValue, int? baseValue = null)
+    {
+        if (string.IsNullOrWhiteSpace(expression))
+        {
+            return defaultValue;
+        }
+
+        object evaluated = InvokeRpnEvaluator(
+            expression,
+            new Dictionary<string, int>
+            {
+                { "wave", currentWave },
+                { "base", baseValue ?? defaultValue }
+            });
+
+        return Convert.ToInt32(evaluated);
+    }
+
+    object InvokeRpnEvaluator(string expression, Dictionary<string, int> variables)
+    {
+        Type rpnType = Type.GetType("RPNEvaluator.RPN, RPNEvaluator")
+            ?? Type.GetType("RPNEvaluator.RPNEvaluator, RPNEvaluator");
+
+        if (rpnType == null)
+        {
+            throw new InvalidOperationException("Could not find the RPNEvaluator type in RPNEvaluator.dll.");
+        }
+
+        MethodInfo method = rpnType
+            .GetMethods(BindingFlags.Public | BindingFlags.Static)
+            .FirstOrDefault(candidate =>
+            {
+                if (candidate.Name != "Evaluate")
+                {
+                    return false;
+                }
+
+                ParameterInfo[] parameters = candidate.GetParameters();
+                return parameters.Length == 2
+                    && parameters[0].ParameterType == typeof(string)
+                    && parameters[1].ParameterType.IsAssignableFrom(typeof(Dictionary<string, int>));
+            });
+
+        if (method == null)
+        {
+            throw new InvalidOperationException("Could not find a compatible Evaluate method in RPNEvaluator.dll.");
+        }
+
+        return method.Invoke(null, new object[] { expression, variables });
     }
 }
