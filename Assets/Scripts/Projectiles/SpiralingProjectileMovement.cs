4071dd75b3897ea3f1014f3435b533fb0d63ef72
runp12123 <runpungpuang@gmail.com>
2026-06-09 01:16:49 -0700

    Fix spiral movement

--- Assets/Scripts/Projectiles/SpiralingProjectileMovement.cs
+++ Assets/Scripts/Projectiles/SpiralingProjectileMovement.cs
@@ -3,14 +3,27 @@ using UnityEngine;
 public class SpiralingProjectileMovement : ProjectileMovement
 {
     public float start;
+    public float lastOffset;
+
     public SpiralingProjectileMovement(float speed) : base(speed)
     {
         start = Time.time;
+        lastOffset = 0f;
     }
 
     public override void Movement(Transform transform)
     {
-        transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0), Space.Self);
-        transform.Rotate(0, 0, speed *Mathf.Sqrt(speed)* Time.deltaTime*20.0f/(1 + Random.value + Time.time - start));
+        float timeAlive = Time.time - start;
+        float spiralSpeed = speed * 2f;
+        float spiralSize = 0.3f;
+
+        float offset = Mathf.Sin(timeAlive * spiralSpeed) * spiralSize;
+        float offsetChange = offset - lastOffset;
+        lastOffset = offset;
+
+        Vector3 forwardMove = transform.forward * speed * Time.deltaTime;
+        Vector3 sideMove = transform.right * offsetChange;
+
+        transform.position += forwardMove + sideMove;
     }
 }
