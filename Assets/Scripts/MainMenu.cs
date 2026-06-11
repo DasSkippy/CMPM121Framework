fe99040998219938ca8babdc1231728d9ed48eed
DasSkippy <sean.frederico105@gmail.com>
2026-06-10 19:30:26 -0700

    added comments to main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,22 +4,27 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject mainUI, creditsUI;
+
+    //Called when play is clicked
     public void Play()
     {
         SceneManager.LoadScene("Main");
     }
 
+    //Called when credits is clicked
     public void Credits()
     {
         mainUI.SetActive(false);
         creditsUI.SetActive(true);
     }
 
+    //Called when quit game is called
     public void QuitGame()
     {
         Application.Quit();
     }
 
+    //Called when back is clicked in the credits menu
     public void Back()
     {
         mainUI.SetActive(true);
